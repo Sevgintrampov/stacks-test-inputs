21d0965a69bb1ec9527be02567ff6f43bd985f84
Sevgintrampov <168405248+Sevgintrampov@users.noreply.github.com>
2025-12-08 15:53:48 +0200

    Update main.tf

--- null/main.tf
+++ null/main.tf
@@ -1,1 +1,8 @@
 resource "null_resource" "test" {}
+variable "aloda" {
+  description = "Complex k8s object passed from Stacks"
+  type = map(object({
+    current_kubernetes_version = map(string)
+    id                         = map(string)
+  }))
+}
